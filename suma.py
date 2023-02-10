53e445e33ff22cbbf9afe861f42b31dd4b49e69f
marceloaguierre50 <marceloaguierre@github.com>
2023-02-10 18:27:56 -0300

    modificado recientemente06 10 23

--- suma.py
+++ suma.py
@@ -1,4 +1,21 @@
-x=5
-y =3
-suma= x +y
-print("la suma:") and suma
+"""x = float(input("ingrese numero 1:"))
+y = float(input("ingrese numero 2:"))
+suma= x + y
+resta= x -y
+multiplicacion = x * y
+divi = x / y
+
+print("la suma es:", suma)
+print("la resta es:", resta)
+print("la multiplicacion es:",multiplicacion)
+print("la divicion es:", divi)
+"""
+def suma (x,y):
+    suma = x + y
+    print ("la suma es:" ,suma)
+suma(6,6)
+suma(5,1)
+
+
+
+
